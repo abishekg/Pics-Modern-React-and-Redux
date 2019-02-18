22c54ec74a16f74facbd163672229c925c501a53
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-18 11:00:45 +0530

    SearchBar Value

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import SearchBar from './SearchBar';
 
 
 export class App extends Component {
+
+  onSearchSubmit(term) {
+    console.log('term: ', term);
+
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     )
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,20 +3,26 @@ import React, { Component } from 'react'
 export class SearchBar extends Component {
 
   state = {
-    searchBarText: 'asdfas'
+    term: ''
   }
 
-  onInputChange(e) {
-    console.log(e.target.value);
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} value={this.state.searchBarText} />
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
           </div>
         </form>
       </div>
